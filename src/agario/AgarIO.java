bfa5bd74aa4cc433a71b03bd2e51a67579e44845
Vigneet Sompura <vigneetsompura@gmail.com>
2019-03-29 22:20:52 -0400

    Add game loop

--- src/agario/AgarIO.java
+++ src/agario/AgarIO.java
@@ -43,6 +43,38 @@ public class AgarIO extends Canvas implements Runnable{
 	}
 	@Override
 	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now-lastTime)/ns;
+			lastTime = now;
+			while(delta >= 1) {
+				tick();
+				delta--;
+			}
+			if(running)
+				render();
+			frames++;
+			if(System.currentTimeMillis()-timer > 1000) {
+				timer += 1000;
+				System.out.println("FPS: "+frames);
+				frames = 0;
+			}
+		}
+		stop();
+	}
+
+	private void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void tick() {
 		// TODO Auto-generated method stub
 		
 	}
