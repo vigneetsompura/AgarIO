1ccce6c8f517814cff3347521dbc850c4a8e41f6
Vigneet Sompura <vigneetsompura@gmail.com>
2019-04-08 15:32:57 -0400

    added boost mechanism

---
+++ src/agario/KeyInput.java
@@ -0,0 +1,27 @@
+package agario;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyInput extends KeyAdapter {
+
+	Player p;
+	boolean flag = true;
+	
+	public KeyInput(Player p) {
+		this.p = p;
+	}
+	
+	public void keyPressed(KeyEvent k) {
+		if(k.getKeyCode()==32 && flag) {
+			p.setBoost(10);
+			flag = false;
+		}
+	}
+	
+	public void keyReleased(KeyEvent k) {
+		p.setBoost(1);
+		flag=true;
+	}
+
+}
