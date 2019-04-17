047b2d66b6de05efbb300475900be4d4511701f0
ankitprahladsoni <ankitprahladsoni@gmail.com>
2019-04-17 15:03:31 -0400

    create smaller methods

--- src/client/Client.java
+++ src/client/Client.java
@@ -169,10 +169,6 @@ public class Client extends Canvas implements Runnable {
         bs.show();
     }
 
-    public Game getGame() {
-        return game;
-    }
-
     public void setGame(Game game) {
         this.game = game;
     }
--- src/client/ResponseHandler.java
+++ src/client/ResponseHandler.java
@@ -4,6 +4,7 @@ import agario.Game;
 import agario.Player;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -17,7 +18,7 @@ public class ResponseHandler implements Runnable {
     private DatagramSocket clientSocket;
     private byte[] inData = new byte[65500];
 
-    public ResponseHandler(DatagramSocket clientSocket, Client client) throws UnknownHostException, SocketException {
+    public ResponseHandler(DatagramSocket clientSocket, Client client) {
         this.client = client;
         this.clientSocket = clientSocket;
     }
@@ -25,22 +26,26 @@ public class ResponseHandler implements Runnable {
     @Override
     public void run() {
         try {
-            DatagramPacket in = new DatagramPacket(inData, inData.length);
-            clientSocket.receive(in);
-
-            inData = in.getData();
-
-            ObjectInputStream objStream = new ObjectInputStream(new ByteArrayInputStream(inData));
-            Game game = (Game) objStream.readObject();
-            if (game.getPlayer(client.getPlayerHandler().getPlayerID()) != null) {
+            Game game = readObjectFromSocket();
+            int playerID = client.getPlayerHandler().getPlayerID();
+            if (game.getPlayer(playerID) != null) {
                 client.setGame(game);
-                client.getPlayerHandler().setRadius(game.getPlayer(client.getPlayerHandler().getPlayerID()).getRadius());
+                client.getPlayerHandler().setRadius(game.getPlayer(playerID).getRadius());
             } else {
                 client.stop();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private Game readObjectFromSocket() throws IOException, ClassNotFoundException {
+        DatagramPacket in = new DatagramPacket(inData, inData.length);
+        clientSocket.receive(in);
+        inData = in.getData();
+
+        ObjectInputStream objStream = new ObjectInputStream(new ByteArrayInputStream(inData));
+        return (Game) objStream.readObject();
     }
 
     public void start() {
