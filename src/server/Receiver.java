ea828f8a27bbfb97839150ff94bd6a45e26c6701
ankitprahladsoni <ankitprahladsoni@gmail.com>
2019-04-17 16:16:39 -0400

    slipt commands to methods

--- src/server/Receiver.java
+++ src/server/Receiver.java
@@ -18,18 +18,18 @@ public class Receiver implements Runnable {
     private byte[] messageBytes = new byte[65500];
     private Thread thread;
 
-    public Receiver(Handler handler) throws SocketException {
+    Receiver(Handler handler) throws SocketException {
         socket = new DatagramSocket(4445);
         this.handler = handler;
     }
 
-    synchronized public void start() {
+    synchronized void start() {
         thread = new Thread(this);
         running = true;
         thread.start();
     }
 
-    synchronized public void stop() {
+    synchronized void stop() {
         try {
             running = false;
             thread.join();
@@ -42,55 +42,71 @@ public class Receiver implements Runnable {
     public void run() {
         System.out.println("Server thread started.");
         while (running) {
-            DatagramPacket packet = new DatagramPacket(messageBytes, messageBytes.length);
-            try {
-                socket.receive(packet);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            DatagramPacket packet = readPacket();
 
-            InetAddress address = packet.getAddress();
             int port = packet.getPort();
             String received = new String(packet.getData(), 0, packet.getLength());
             String command = received.split(":")[0];
             String parameters = received.split(":")[1];
-
-            switch (command) {
-                case "startGame": {
-                    int id = Integer.parseInt(parameters.trim());
-                    Random random = new Random();
-                    Player newPlayer = new Player(id, random.nextInt(Game.WIDTH - 64) + 32, random.nextInt(Game.HEIGHT - 64) + 32);
-                    handler.addPlayer(newPlayer);
-                    System.out.println("New player joined: " + id);
-                    //send handler to player
-                    Sender sender = new Sender(socket, address, port, handler);
-                    Thread thread = new Thread(sender);
-                    thread.start();
-
-                    break;
-                }
-                case "locationUpdate":
-                    String[] p = parameters.split(",");
-                    Player player = handler.getPlayer(Integer.parseInt(p[0].trim()));
-                    if (player != null) {
-                        player.setXY(Integer.parseInt(p[1].trim()), Integer.parseInt(p[2].trim()));
-                        //send handler to player
-                        Sender sender = new Sender(socket, address, port, handler);
-                        Thread thread = new Thread(sender);
-                        thread.start();
-                    }
-
-                    break;
-                case "endGame": {
-                    int playerID = Integer.parseInt(parameters);
-                    handler.removePlayer(playerID);
-                    Sender sender = new Sender(socket, address, port, handler);
-                    Thread thread = new Thread(sender);
-                    thread.start();
-                    break;
-                }
-            }
+            InetAddress address = packet.getAddress();
+            executeCommand(port, command, parameters, address);
         }
         socket.close();
+    }
+
+    private void executeCommand(int port, String command, String parameters, InetAddress address) {
+        switch (command) {
+            case "startGame": {
+                startGame(address, port, parameters);
+                break;
+            }
+            case "locationUpdate":
+                locationUpdate(address, port, parameters);
+                break;
+            case "endGame": {
+                endGame(address, port, parameters);
+                break;
+            }
+        }
+    }
+
+    private DatagramPacket readPacket() {
+        DatagramPacket packet = new DatagramPacket(messageBytes, messageBytes.length);
+        try {
+            socket.receive(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return packet;
+    }
+
+    private void startGame(InetAddress address, int port, String parameters) {
+        int id = Integer.parseInt(parameters.trim());
+        Random random = new Random();
+        Player newPlayer = new Player(id, random.nextInt(Game.WIDTH - 64) + 32, random.nextInt(Game.HEIGHT - 64) + 32);
+        handler.addPlayer(newPlayer);
+        System.out.println("New player joined: " + id);
+        startSenderThread(address, port);
+    }
+
+    private void locationUpdate(InetAddress address, int port, String parameters) {
+        String[] p = parameters.split(",");
+        Player player = handler.getPlayer(Integer.parseInt(p[0].trim()));
+        if (player != null) {
+            player.setXY(Integer.parseInt(p[1].trim()), Integer.parseInt(p[2].trim()));
+            startSenderThread(address, port);
+        }
+    }
+
+    private void endGame(InetAddress address, int port, String parameters) {
+        int playerID = Integer.parseInt(parameters);
+        handler.removePlayer(playerID);
+        startSenderThread(address, port);
+    }
+
+    private void startSenderThread(InetAddress address, int port) {
+        Sender sender = new Sender(socket, address, port, handler);
+        Thread thread = new Thread(sender);
+        thread.start();
     }
 }
